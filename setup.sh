fa29dcc98a9c5c1665dc162057bd3f9a6d6b7246
Suluh Sulistiawan <20874779+sooluh@users.noreply.github.com>
2022-01-30 18:37:40 +0700

    :running_man: enable pm2 at startup

--- setup.sh
+++ setup.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
 
-# Change dir to /tmp
+# change dir to /tmp
 cd /tmp
 
-# Update & upgrade packages
+# update & upgrade packages
 sudo apt update
 sudo apt upgrade -y
 
-# Install net-tools
+# snstall net-tools
 sudo apt install -y net-tools
 
-# Install development tools
+# install development tools
 sudo apt install -y build-essential
 
-# Install NVM
+# install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-# Install Node.js
+# install node.js
 nvm install --lts
 npm i -g npm
 
-# Install pm2
+# install pm2
 npm i -g pm2
 
-# Install LibreOffice
+# enable pm2 at startup
+pm2 startup
+
+# install libreoffice
 sudo apt install -y libreoffice
 
-# Install wkhtmltopdf
+# install wkhtmltopdf
 wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb
 sudo apt install -y ./wkhtmltox_0.12.6-1.focal_amd64.deb
 rm -rf ./wkhtmltox_0.12.6-1.focal_amd64.deb
