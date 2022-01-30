f1ef1cc7aeb4f03957885582c8a3bd60a2b8d113
Suluh Sulistiawan <20874779+sooluh@users.noreply.github.com>
2022-01-30 17:18:33 +0700

    :triangular_flag_on_post: add `-y` flag to wkhtmltopdf installation

--- setup.sh
+++ setup.sh
@@ -27,7 +27,7 @@ sudo apt install -y libreoffice
 
 # Install wkhtmltopdf
 wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb
-sudo apt install ./wkhtmltox_0.12.6-1.focal_amd64.deb
+sudo apt install -y ./wkhtmltox_0.12.6-1.focal_amd64.deb
 rm -rf ./wkhtmltox_0.12.6-1.focal_amd64.deb
 
 # autoremove & autoclean
