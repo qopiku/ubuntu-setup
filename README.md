5c7a4e3ec1b059a24cc3d1f9c99d5bb059b59d5b
Sool Uh! <suluh.webdevelopers@hotmail.com>
2022-01-30 17:01:51 +0700

    :imp: updatee

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # VPS Setup :computer:
 
-```
+```bash
 curl -o- https://raw.githubusercontent.com/sProDev/vps/main/setup.sh | bash
 ```
--- setup.sh
+++ setup.sh
@@ -13,8 +13,11 @@ sudo apt install -y build-essential
 # Install Node.js
 # via NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-source ~/.bashrc
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 nvm install --lts
+npm i -g npm
 
 # autoremove & autoclean
 sudo apt autoremove -y
