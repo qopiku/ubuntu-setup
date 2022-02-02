41136f6a634e16e06a058d75a3219267f7cd95fc
Suluh Sulistiawan <20874779+sooluh@users.noreply.github.com>
2022-02-02 15:44:34 +0700

    :robot: add pdfbot installation
    link pm2 with pm2+ monit
    save current dir and return it at the end

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# save current directory to variable
+CURR_DIR=$(pwd)
+
 # change dir to /tmp
 cd /tmp
 
@@ -25,8 +28,9 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 nvm install --lts
 npm i -g npm
 
-# install pm2
+# install pm2 & connect to pm2+ monit
 npm i -g pm2
+pm2 link ${PM2_SECRET_KEY} ${PM2_PUBLIC_KEY}
 
 # enable pm2 at startup
 pm2 startup
@@ -39,6 +43,21 @@ wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmlt
 sudo apt install -y ./wkhtmltox_0.12.6-1.focal_amd64.deb
 rm -rf ./wkhtmltox_0.12.6-1.focal_amd64.deb
 
+# change dir to /opt
+cd /opt
+
+# pdfbot installation
+git clone https://${GH_TOKEN}@github.com/sooluh/pdfbot-private.git --single-branch --branch build pdfbot
+cd pdfbot
+npm ci --unsafe-perm=true
+pm2 start npm --name "pdfbot" -- start
+
+# change dir to /tmp
+cd /tmp
+
 # autoremove & autoclean
 sudo apt autoremove -y
 sudo apt autoclean
+
+# change dir to CURR_DIR
+cd ${CURR_DIR}
