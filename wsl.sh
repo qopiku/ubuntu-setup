d06d177965c443ece35d97160611558f84a81651
Suluh Sulistiawan <20874779+sooluh@users.noreply.github.com>
2022-02-14 10:45:26 +0700

    :computer: create wsl.sh

---
+++ wsl.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# save current directory to variable
+CURR_DIR=$(pwd)
+
+# change dir to /tmp
+cd /tmp
+
+# update & upgrade packages
+sudo apt update
+sudo apt upgrade -y
+
+# install net-tools
+sudo apt install -y net-tools
+
+# install development tools
+sudo apt install -y build-essential
+
+# install telnet
+sudo apt install -y telnet
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# install node.js
+nvm install --lts
+npm i -g npm
+
+# install personal utility
+npm i -g yarn pnpm vercel typescript concurrently
+
+# install libreoffice
+sudo apt install -y libreoffice
+
+# install wkhtmltopdf
+wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb
+sudo apt install -y ./wkhtmltox_0.12.6-1.focal_amd64.deb
+rm -rf ./wkhtmltox_0.12.6-1.focal_amd64.deb
+
+# autoremove & autoclean
+sudo apt autoremove -y
+sudo apt autoclean
+
+# change dir to CURR_DIR
+cd ${CURR_DIR}
