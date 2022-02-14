be93f8ebcfe10cad643915b8af749452f8cb174c
Suluh Sulistiawan <20874779+sooluh@users.noreply.github.com>
2022-02-14 11:11:43 +0700

    :bike: minimize comments

--- vps.sh
+++ vps.sh
@@ -19,16 +19,10 @@ sudo apt install -y build-essential
 # install telnet
 sudo apt install -y telnet
 
-# install dependencies (docker)
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
-
-# add docker gpg key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-# install docker repository
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
-
 # install docker
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
 sudo apt update
 sudo apt-get install docker-ce
 
@@ -36,10 +30,8 @@ sudo apt-get install docker-ce
 sudo systemctl start docker
 sudo systemctl enable docker
 
-# download latest docker-compose
+# install docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-# change file permission
 sudo chmod +x /usr/local/bin/docker-compose
 
 # install nvm
