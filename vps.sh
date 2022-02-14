5069581749d2b2278765f8d6816449be237042ed
Suluh Sulistiawan <20874779+sooluh@users.noreply.github.com>
2022-02-14 11:14:45 +0700

    :crab: got rid of apt-get and made auto yes

--- vps.sh
+++ vps.sh
@@ -20,11 +20,11 @@ sudo apt install -y build-essential
 sudo apt install -y telnet
 
 # install docker
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable" -y
 sudo apt update
-sudo apt-get install docker-ce
+sudo apt install -y docker-ce
 
 # enable docker service
 sudo systemctl start docker
