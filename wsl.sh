7634f77d2f127506f064be7f7c3760ddfc9f289c
Suluh Sulistiawan <20874779+sooluh@users.noreply.github.com>
2022-03-13 22:07:58 +0700

    :memo: uninstalled storj.io and added installation for zip and unzip

--- wsl.sh
+++ wsl.sh
@@ -19,6 +19,9 @@ sudo apt install -y build-essential
 # install telnet
 sudo apt install -y telnet
 
+# install zip unzip
+sudo apt install -y zip unzip
+
 # install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
@@ -38,11 +41,6 @@ sudo apt install -y libreoffice
 wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb
 sudo apt install -y ./wkhtmltox_0.12.6-1.focal_amd64.deb
 rm -rf ./wkhtmltox_0.12.6-1.focal_amd64.deb
-
-# install uplink cli (storj.io)
-curl -L https://github.com/storj/storj/releases/latest/download/uplinkng_linux_amd64.zip -o uplinkng_linux_amd64.zip
-unzip -o uplinkng_linux_amd64.zip
-sudo install uplinkng /usr/local/bin/uplink
 
 # install ngrok
 curl -L https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz
